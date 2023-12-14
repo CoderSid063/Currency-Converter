7293eb7888caf4bab15dbfe74ce064a3f8671efa
CoderSid063 <sudhansupanda063@gmail.com>
2023-12-14 20:06:20 +0530

    feat : implement all function of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import "./App.css";
+import { InputBox } from "./components";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  };
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo);
+  };
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Currency App</h1>
+      <InputBox />
     </>
   );
 }
